3f097ed67ea256444c912d1c8ebf317a17e1f67b
anupamsingh81 <anupampom@gmail.com>
2017-09-21 12:25:50 +0530

    hhh

--- recode.R
+++ recode.R
@@ -10,7 +10,58 @@ summary(dat)
 summary(dat$y)
 summary(as.factor(dat$y))
 
+# R
+
+library(dplyr)
 dat$y = recode(dat$y, a = "Apple", .default = levels(dat$y))
 
+#dplyr
+
+# Replace characters/Factors with recode
 
 dat %>% mutate(y= recode(y, b = "Bat", .default = levels(y)))
+
+dat %>% mutate(y= recode(y, b = "Bat", .default = levels(y)))
+
+dat %>% mutate(y= recode(y, c = "Cat", .default = levels(y)))
+
+
+# you can use dplyr to replace integers , but better to use base
+dat %>% mutate(x= recode(x, `3` =6L))
+
+dat %>% mutate(x= recode(x, 4 =6L)) # always put ``  in left hand side or it wouldnt work and L for integer before 3 or 4 
+
+# Base R
+dat$x[dat$x==5] = 5.2 
+
+
+# Mosaic derived factor, multiple nested if else
+
+library(mosaic)
+
+dat %>% mutate(z = derivedFactor( g=x==2, h= x>3,.ordered=TRUE))
+
+
+dat %>% mutate(z = ifelse(x<=2,"Low",
+                          ifelse(x>=4,"High","Medium")))
+
+
+
+dat %>% mutate(z= derivedFactor(Low = x<=2 ,High = x>=4 ,Medium = !(x<=2|x>=4)))
+
+
+# dplyr case_when 
+# https://www.rdocumentation.org/packages/dplyr/versions/0.7.3/topics/case_when
+
+dat %>% mutate(z1 = case_when(
+  x<=2~"Low",
+  x>=4~"High",
+  TRUE~"Medium"
+))
+
+# For changing factors
+
+
+library(forcats)
+
+mutate(dat, z2 = fct_recode(y, "b" = "f"))
---
+++ variable_selection.R
@@ -0,0 +1,101 @@
+library(Boruta)
+
+traindata = ungl
+traindata[traindata == ""] <- NA # convert all missing valus into NA
+
+summary(traindata)
+
+df <- traindata %>% mutate_if(is.character,as.factor)
+
+traindata = complete.cases(traindata) #complete cases only
+
+# convert all categorical variable into 
+
+
+df = df %>% select( -c(mob.no,X.3,base.line.creatine)) # -select to remove variables
+
+df_complete = df[complete.cases(df),]
+
+boruta.train <- Boruta(outcome30~., data = df_complete, doTrace = 2)
+ print(boruta.train)
+ 
+ summary(boruta.train)
+ 
+ plot(boruta.train, xlab = "", xaxt = "n")
+ 
+ 
+ lz<-lapply(1:ncol(boruta.train$ImpHistory),function(i)
+   boruta.train$ImpHistory[is.finite(boruta.train$ImpHistory[,i]),i])
+  names(lz) <- colnames(boruta.train$ImpHistory)
+  Labels <- sort(sapply(lz,median))
+ axis(side = 1,las=2,labels = names(Labels),
+        at = 1:ncol(boruta.train$ImpHistory), cex.axis = 0.7)
+ 
+ final.boruta <- TentativeRoughFix(boruta.train)
+  print(final.boruta)
+ 
+ final.boruta <- TentativeRoughFix(boruta.train)
+ print(final.boruta)
+ 
+ boruta.train1 = final.boruta
+ plot(boruta.train1, xlab = "", xaxt = "n")
+ 
+ 
+ lz<-lapply(1:ncol(boruta.train1$ImpHistory),function(i)
+   boruta.train1$ImpHistory[is.finite(boruta.train1$ImpHistory[,i]),i])
+ names(lz) <- colnames(boruta.train1$ImpHistory)
+ Labels <- sort(sapply(lz,median))
+ axis(side = 1,las=2,labels = names(Labels),
+      at = 1:ncol(boruta.train1$ImpHistory), cex.axis = 0.7)
+ 
+ getSelectedAttributes(final.boruta, withTentative = F)
+ 
+ boruta.df <- attStats(final.boruta)
+  class(boruta.df)
+  
+  print(boruta.df)
+  
+  # randomeforest
+  
+  library(randomForest)
+  library(caret)
+  modelFit <- train( outcome30~.,data=df_complete, method="rf", importance = TRUE)  
+  varImp(modelFit)
+  
+  
+ # http://rstatistics.net/variable-importance-of-predictors-that-contribute-most-significantly-to-a-response-variable-in-r/
+  
+  glm(outcome30~.)
+  
+  cor(df_complete)
+  
+  library(psych)
+  
+  polychoric(df_complete)
+ 
+  tetrachoric(df_complete)
+  
+  
+  library(tilting)
+  
+  # multiple t test R
+  # https://stackoverflow.com/questions/9661469/r-t-test-over-all-columns
+  
+  
+  library(MASS)
+  
+  df_new = df_complete %>% select(-c(outcome90))
+ 
+  fit <- glm(outcome30~.,data=df_new,family = binomial())
+  step <- stepAIC(fit, direction="both")
+  step$anova # display results 
+  
+  
+  library(leaps)
+ 
+  leaps<-regsubsets(outcome30~.,data=df_new,nbest=10 ,really.big = T)
+  # view results
+  summary(leaps)
+  # plot a table of models showing variables in each model.
+  # models are ordered by the selection statistic.
+  plot(leaps,scale="r2")
